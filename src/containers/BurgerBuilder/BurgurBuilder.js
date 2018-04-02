d580c8c2109b71b2190d9cea99d2c6fdf6fee91a
Zavin Hussein <zavinhussein@gmail.com>
2018-04-02 20:54:34 +0300

    Added BurgurBuilder Component

---
+++ src/containers/BurgerBuilder/BurgurBuilder.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgurBuilder extends Component {
+  
+  render() {
+    return(
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controller</div>
+      </Aux>
+  
+    );
+  }
+  
+  
+}
+
+export default BurgurBuilder;
