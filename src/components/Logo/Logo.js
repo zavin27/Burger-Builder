eb6041e9d7073f89d6e54d69502906c197e3a6fb
Zavin Hussein <zavinhussein@gmail.com>
2018-05-03 19:35:02 +0300

    Added Toolbar and Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BurgerLogo from '../../assets/images/burger-logo.png'
+
+import classes from './Logo.css'
+
+const logo = (props) => (
+  <div className={classes.Logo}>
+    <img src={BurgerLogo} alt="MyBurgerLogo"/>
+  </div>
+);
+
+export default logo;
