33d7175a206a96b9d7c406d05995576b60079a10
Zavin Hussein <zavinhussein@gmail.com>
2018-07-10 01:50:21 +0300

    Added error handling with HOC

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 
 const INGREDIENT_PRICES = {
@@ -201,4 +202,4 @@ class BurgerBuilder extends Component {
   
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
