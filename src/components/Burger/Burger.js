22a650307049dc6e4a46ba24eb4de449e6bfb45b
Zavin Hussein <zavinhussein@gmail.com>
2018-07-28 06:24:18 +0300

    move Check validity function to shared folder remove all console.log

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,6 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 
 const burger = (props) => {
-  // let transformedIngredients = Object.keys(props.ingredients)
-  //   .map(igKey => {
-  //     return [...Array(props.ingredients[igKey])]
-  //       .map((_, i) => {
-  //         return <BurgerIngredient type={igKey} key={igKey + i}/>;
-  //       })
-  //
-  //   })
-  //   .reduce((arr, el) => {
-  //     return arr.concat(el)
-  //   }, []);
   let transformedIngredients = props.ingredientsOrder.map((igKey, i) => {
     return <BurgerIngredient type={igKey} key={igKey + i}
                              remove={(type, index) => props.removeIngredientOnClick(type, index)} index={i}/>;
